3db76b980d7f109e57a652b651cb57b1a80682a2
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-20 16:21:04 -0700

    fixinf task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,11 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_head = malloc(sizeof(list_t));
 
 	if (!head || !new_head)
+	{
+		free(new_head);
 		return (NULL);
+	}
+
 	if (str)
 	{
 		new_head->str = strdup(str);
@@ -21,6 +25,11 @@ list_t *add_node(list_t **head, const char *str)
 			return (NULL);
 		}
 		new_head->len = _strlen(new_head->str);
+	}
+	else
+	{
+		new_head->str = NULL;
+		new_head->len = 0;
 	}
 
 	new_head->next = *head;
