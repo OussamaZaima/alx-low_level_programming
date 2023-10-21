5ee8700dddeb58c62164a7c48e7eed7ebb68c6bf
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-20 18:54:21 -0700

    fixed task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdnoreturn.h>
-
-void _constructor(void) __attribute__ ((constructor));
 
 /**
- * _constructor - executes before main()
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void _constructor(void)
+int main(void)
 {
     printf("(You're beat! and yet, you must allow,\n"
 		    "I bore my house upon my back!\n");
+    return (0);
 }
