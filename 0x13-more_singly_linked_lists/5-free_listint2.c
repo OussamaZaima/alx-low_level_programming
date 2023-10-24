84a9361702eb6274963925d9a6b6eeb12952fd42
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-24 15:00:32 -0700

    task8

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * free_listinttwo - Frees a listint_t list.
+ * free_listint2 - Frees a listint_t list.
  * @head: A pointer to the address of the head of the listint_t list.
  *
  * Description: Sets the head to NULL.
  */
-void free_listinttwo(listint_t **head)
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
