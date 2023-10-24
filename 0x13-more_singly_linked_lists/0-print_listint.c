63c80227d25bb28478c882f7b211bb3658c570f3
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-24 13:55:32 -0700

    fix task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,9 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list.
+ * @h: A pointer to the head of the list_t list.
  *
- * Return: The number of nodes.
+ * Return: The number of nodes in the list_t list.
  */
 size_t print_listint(const listint_t *h)
 {
@@ -18,5 +18,5 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 	}
 
-	return nodes;
+	return (nodes);
 }
