25b8585aa3c9d6eecafddc11076d23a36de65fc9
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-26 16:40:44 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,18 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
-
-	if (n == 0 && index < 64)
-		return (0);
-
-	for (i = 0; i <= 63; n >>= 1, i++)
+	if (index >= 64)
 	{
-		if (index == i)
-		{
-			return (n & 1);
-		}
+		return -1;
 	}
 
-	return (-1);
+	return (n >> index) & 1;
 }
