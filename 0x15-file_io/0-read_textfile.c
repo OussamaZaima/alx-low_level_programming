75ccd0df899ffdd4ae9832f3af2cbf8a00a2220a
oussama zaima <oussama.zaima2002@gmail.com>
2023-10-30 12:06:29 -0700

    fixtask0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -12,28 +10,16 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8]
 
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (bytes);
 }
